0e0b859eb32957d57bbb8acc8a92ee1f3750a78f
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-07 20:47:29 -0300

    Implement remove Pessoa

--- src/main/java/br/com/icaropinho/algamoney/api/resource/PessoaResource.java
+++ src/main/java/br/com/icaropinho/algamoney/api/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.icaropinho.algamoney.api.event.RecursoCriadoEvent;
@@ -48,6 +50,12 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
 		Pessoa pessoa = pessoaRepositorio.findOne(codigo);
 		 return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable("codigo") Long codigo) {
+		pessoaRepositorio.delete(codigo);
 	}
 	
 }
