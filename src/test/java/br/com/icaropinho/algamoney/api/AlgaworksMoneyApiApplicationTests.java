923b0b98e67bf5c0fdefac59bab81233924f6a3a
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-07 18:42:16 -0300

    Initial commit

---
+++ src/test/java/br/com/icaropinho/algamoney/api/AlgaworksMoneyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.icaropinho.algamoney.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgaworksMoneyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
