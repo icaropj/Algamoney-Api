98cfe2036428c632cf4f4e801042b80f59f8c244
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-07 19:19:25 -0300

    Implement insert and findById on CategoriaResource

--- src/main/java/br/com/icaropinho/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/br/com/icaropinho/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,17 @@
 package br.com.icaropinho.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.icaropinho.algamoney.api.model.Categoria;
 import br.com.icaropinho.algamoney.api.repositorio.CategoriaRepositorio;
@@ -20,6 +26,24 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepositorio.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaRepositorio.save(categoria);
+		URI uri = ServletUriComponentsBuilder
+			.fromCurrentRequestUri()
+			.path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo())
+			.toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable("codigo") Long codigo) {
+		Categoria categoria = categoriaRepositorio.findOne(codigo);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 	
 }
