ecc429784a43e29ba0b323c7b39d272453bf2fb8
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-09 01:42:39 -0300

    Implement delete for Lancamento

--- src/main/java/br/com/icaropinho/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/br/com/icaropinho/algamoney/api/resource/LancamentoResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.icaropinho.algamoney.api.event.RecursoCriadoEvent;
@@ -51,6 +53,12 @@ public class LancamentoResource {
 		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable("codigo") Long codigo) {
+		lancamentoRepositorio.delete(codigo);
 	}
 	
 }
