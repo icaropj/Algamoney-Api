b7f2fc9de3526ba3d74ef259e326d2733932bbdd
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-08 23:00:08 -0300

    Delete Pessoa

--- src/main/java/br/com/icaropinho/algamoney/api/resource/PessoaResource.java
+++ src/main/java/br/com/icaropinho/algamoney/api/resource/PessoaResource.java
@@ -5,14 +5,17 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,6 +59,17 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable("codigo") Long codigo) {
 		pessoaRepositorio.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@RequestBody @Valid Pessoa pessoa, @PathVariable("codigo") Long codigo){
+		Pessoa pessoaSalva = pessoaRepositorio.findOne(codigo);
+		if(pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		pessoaRepositorio.save(pessoaSalva);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 	
 }
