4c5d38b2b3695552ece11f0b49a19875b411011d
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-11 16:17:53 -0300

    Implement Pessoa filter

--- src/main/java/br/com/icaropinho/algamoney/api/repositorio/PessoaRepositorio.java
+++ src/main/java/br/com/icaropinho/algamoney/api/repositorio/PessoaRepositorio.java
@@ -1,5 +1,8 @@
 package br.com.icaropinho.algamoney.api.repositorio;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import br.com.icaropinho.algamoney.api.model.Pessoa;
 
 @Repository
 public interface PessoaRepositorio extends JpaRepository<Pessoa, Long> {
+
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
