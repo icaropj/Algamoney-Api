faddc2ce59661fc46789c8de62c64ae4e62ca338
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2018-03-11 13:05:41 -0300

    Implement logout

---
+++ src/main/java/br/com/icaropinho/algamoney/api/resource/TokenResource.java
@@ -0,0 +1,34 @@
+package br.com.icaropinho.algamoney.api.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Classe para fazer logout -> Simplesmente tirar o valor de refreshToken
+ * @author icaro
+ *
+ */
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false); //TODO: Em produção será true
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+	
+}
